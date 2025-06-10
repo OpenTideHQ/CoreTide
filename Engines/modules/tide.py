5c767d8dfc6a0782a1d22d27e28deff0c69430d8
BESSON Amine (DIGIT-EXT) <amine.besson@ext.ec.europa.eu>
2025-06-10 11:22:42 +0200

    debug: check sentinel loading

--- Engines/modules/tide.py
+++ Engines/modules/tide.py
@@ -271,7 +271,8 @@ class SystemLoader:
         Sentinel = TideModels.MDR.Configurations.Sentinel
 
         mdr_config, base_config = SystemLoader._base_configuration(mdr_config)
-
+        print("DEBUG")
+        print(mdr_config)
         query = mdr_config.pop("query")
         trigger = mdr_config.pop("trigger", None)
         if trigger:

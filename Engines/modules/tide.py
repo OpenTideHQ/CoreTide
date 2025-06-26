4d66ef4b0e04797a3946359d0aac3b8111d115ad
BESSON Amine (DIGIT-EXT) <amine.besson@ext.ec.europa.eu>
2025-06-26 17:03:16 +0200

    fix: entities data model when no entities are present

--- Engines/modules/tide.py
+++ Engines/modules/tide.py
@@ -300,6 +300,7 @@ class SystemLoader:
                                          alert=alert_grouping)
 
         entity_list = mdr_config.pop("entities", None)
+        entities = None
         if entity_list:
             entities = []
             for mapping in entity_list:

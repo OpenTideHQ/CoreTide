263ec73bcaf04da7a6b416e243fd8db1122ee90e
BESSON Amine (DIGIT-EXT) <amine.besson@ext.ec.europa.eu>
2025-10-22 20:35:37 +0200

    fix: default value for signals if missing in index

--- Engines/modules/tide.py
+++ Engines/modules/tide.py
@@ -1131,7 +1131,7 @@ class DataTide:
         """Detection Objectives Raw Index"""
         DOM = {uuid:TideLoader.load_dom(deepcopy(data)) for (uuid, data) in dict(Index.copy().get("dom", {})).items()} if dom else None
         """Detection Objectives Pre-Loaded Index"""
-        signal = dict(Index["signal"])
+        signal = dict(Index.get("signal", {}))
         """Detection Objectives Signals Raw Index"""
         Signal = {uuid:TideLoader.load_signal(deepcopy(data)) for (uuid, data) in dict(Index.copy().get("signal", {})).items()} if signal else None 
         """Detection Objectives Signals Pre-Loaded Index"""
